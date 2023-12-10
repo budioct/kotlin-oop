54f9059d344c8da8eb680e28a9b30b3783fe4925
Budhi Octaviansyah <budioct@gmail.com>
2023-12-10 14:06:13 +0700

    create initializer block

--- src/main/kotlin/data/Person.kt
+++ src/main/kotlin/data/Person.kt
@@ -6,6 +6,13 @@ class Person(
     lastName: String
 ) {
 
+    // block body class tidak sembarangan bisa digunakan, untuk itu jika ingin menjalankan program butuh.. saran jangan code terlalu berat
+    // initializer block (akan jalan ketika constractor nya di panggil)
+    init {
+        println("Initializer Block")
+        println("Person $firstName dibuat")
+    }
+
     var firstName: String = firstName
     var middleName: String? = middleName
     var lastName: String = lastName
