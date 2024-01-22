e526653b991c6a8b18ad1aa97d7a9e092c36b362
Budhi Octaviansyah <budioct@gmail.com>
2024-01-22 12:56:30 +0700

    super constractor

--- src/main/kotlin/data/Customer.kt
+++ src/main/kotlin/data/Customer.kt
@@ -2,12 +2,12 @@ package data
 
 // class with primary constructor
 open class Customer(val name: String, val type: String, val balance: Long) {
-    constructor(name: String, type: String) : this(name, type, 0) // secondary constructor 1
+    constructor(name: String, type: String) : this(name, type, 0) // secondary constructor 1, call primary constructor
     constructor(name: String) : this(name, "Standart") // // secondary constructor 2
 }
 
 class PremiumCustomer : Customer {
-    constructor(name: String) : super(name, "Premium") // secondary constructor 1 access super from parent class
+    constructor(name: String) : super(name, "Premium") // secondary constructor 1, call super constructor from parent class
     constructor(name: String, balance: Long) : super(name, "Premium", balance) // secondary constructor 1 access super from parent class
 }
 
