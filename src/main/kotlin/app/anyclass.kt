caa9e756e30582d682e9f937d778d8732f3f3e19
Budhi Octaviansyah <budioct@gmail.com>
2024-01-22 13:03:29 +0700

    Any Class

--- src/main/kotlin/app/anyclass.kt
+++ src/main/kotlin/app/anyclass.kt
@@ -7,9 +7,11 @@ fun main() {
 
     val smartPhone = SmartPhone("samsung", "android")
     println("name: ${smartPhone.name} os: ${smartPhone.os}")
+    println(smartPhone.hashCode()) // access method AnyClass
 
     val laptop = Laptop("acer", "linux")
     println("name: ${laptop.name} os: ${laptop.os}")
+    println(laptop.hashCode()) // access method AnyClass
 
 
 }
--- src/main/kotlin/data/Laptop.kt
+++ src/main/kotlin/data/Laptop.kt
@@ -8,6 +8,7 @@ class SmartPhone(val name: String, val os: String) // => class SmartPhone : Any(
  * Any Class
  * ● Di Kotlin, semua class yang kita buat tanpa class Parent, sebenarnya secara otomatis dia akan menjadi class child dari class Any
  * ● Any adalah superclass untuk semua class yang kita buat di Kotlin
+ * mirip seperti di JAVA jika class di buat maka dia adalah child dari class Object
  *
  * public open class Any ---> adalah super class/object dari setiap object yang di buat kan inherited. kalau di java super class Object
  */
