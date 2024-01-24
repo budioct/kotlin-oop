2b25f0ed911d5f9ff171abc80b353de5272116c8
Budhi Octaviansyah <budioct@gmail.com>
2024-01-24 15:46:41 +0700

    reflection update

--- src/main/kotlin/app/reflection.kt
+++ src/main/kotlin/app/reflection.kt
@@ -25,21 +25,21 @@ import exception.ValidationException
 // reflection dynamic
 fun validateRequest(request: Any) {
     val clazz = request::class
-    val properties = clazz.memberProperties
+//    val properties = clazz.memberProperties
 
     // iterate satu satu, lalu cek kalo ada annotation @NotBlank
-    for (property in properties) {
-        if (property.findAnnotation<NotBlank>() != null) {
-            val value = property.call(request)
-            when (value) {
-                is String -> {
-                    if ("" == value) {
-                        throw ValidationException("${property.name} is blank")
-                    }
-                }
-            }
-        }
-    }
+//    for (property in properties) {
+//        if (property.findAnnotation<NotBlank>() != null) {
+//            val value = property.call(request)
+//            when (value) {
+//                is String -> {
+//                    if ("" == value) {
+//                        throw ValidationException("${property.name} is blank")
+//                    }
+//                }
+//            }
+//        }
+//    }
 }
 
 fun main() {
